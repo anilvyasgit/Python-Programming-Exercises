ff728dd0a0636f7a69a8460b82b7657a22d67943
anilvyasgit <56534734+anilvyasgit@users.noreply.github.com>
2019-10-14 13:20:44 +0530

    Update exercise2.py

--- Beginner/exercise2.py
+++ Beginner/exercise2.py
@@ -1,6 +1,15 @@
+#to input any integer
 n=int(input())
+
+#share a number
 ans=n
+
+#facorial development
+#progressive multiplication
+
 while n>1:
     n-=1
     ans=ans*(n)
+
+#ultimate answer
 print(ans)
